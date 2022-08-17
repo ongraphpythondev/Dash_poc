9f26c197ade0d0cb44745cb8db87586079a3ff3d
ongraphpythondev <pythondev@ongraph.com>
2022-08-17 10:44:23 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Dash
+
+### Setup django:
+* Clone repository
+```sh
+git clone https://github.com/ongraphpythondev/Dash_poc.git
+```
+* Create virtual environment
+```sh
+python3 -m venv myenv
+```
+* Activate virtual environment
+```sh
+source myenv/bin/activate
+```
+* Change directory
+```sh
+cd Dash_poc
+```
+* Install all the dependencies:
+```sh
+pip install -r requirements.txt
+```
+* Run server:
+```sh
+python3 app.py
+```
---
+++ app.py
@@ -0,0 +1,180 @@
+from dash import Dash, dcc,html, Input, Output
+import pandas as pd
+import plotly.express as px
+
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77d6363a2d754b59/raw/c353e8ef842413cae56ae3920b8fd78468aa4cb2/usa-agricultural-exports-2011.csv')
+df2 = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+
+df3 = pd.read_csv('https://plotly.github.io/datasets/country_indicators.csv')
+
+fig = px.scatter(df2, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+app = Dash(__name__)
+
+all_options = {
+    'America': ['New York City', 'San Francisco', 'Cincinnati'],
+    'Canada': [u'Montréal', 'Toronto', 'Ottawa']
+}
+
+markdown_text = '''
+### Dash and Markdown
+# Dash and Markdown
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first **introduction to Markdown!**
+'''
+
+app.layout = html.Div([
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df),
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure=fig
+    ),
+    dcc.Markdown(children=markdown_text),
+    html.H6("Change the value in the text box to see callbacks in action!"),
+    html.Div([
+        "Input: ",
+        dcc.Input(id='my-input', value='initial value', type='text')
+    ]),
+    html.Br(),
+    html.Div(id='my-output', children='Hello world'),
+    html.Div([
+
+        html.Div([
+            dcc.Dropdown(
+                df3['Indicator Name'].unique(),
+                'Fertility rate, total (births per woman)',
+                id='xaxis-column'
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='xaxis-type',
+                inline=True
+            )
+        ], style={'width': '48%', 'display': 'inline-block'}),
+
+        html.Div([
+            dcc.Dropdown(
+                df3['Indicator Name'].unique(),
+                'Life expectancy at birth, total (years)',
+                id='yaxis-column'
+            ),
+            dcc.RadioItems(
+                ['Linear', 'Log'],
+                'Linear',
+                id='yaxis-type',
+                inline=True
+            )
+        ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+    ]),
+
+    dcc.Graph(id='indicator-graphic'),
+
+    dcc.Slider(
+        df3['Year'].min(),
+        df3['Year'].max(),
+        step=None,
+        id='year--slider',
+        value=df3['Year'].max(),
+        marks={str(year): str(year) for year in df3['Year'].unique()},
+
+    ),
+
+    dcc.RadioItems(
+        list(all_options.keys()),
+        'America',
+        id='countries-radio',
+    ),
+
+    html.Hr(),
+
+    dcc.RadioItems(id='cities-radio'),
+
+    html.Hr(),
+
+    html.Div(id='display-selected-values')
+])
+
+@app.callback(
+    Output(component_id='my-output', component_property='children'),
+    Input(component_id='my-input', component_property='value')
+)
+def update_output_div1(input_value):
+    return f'Output: {input_value}'
+
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    Input('xaxis-column', 'value'),
+    Input('yaxis-column', 'value'),
+    Input('xaxis-type', 'value'),
+    Input('yaxis-type', 'value'),
+    Input('year--slider', 'value'))
+def update_graph(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type,
+                 year_value):
+    dff = df3[df3['Year'] == year_value]
+
+    fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+                     y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+                     hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
+
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    fig.update_xaxes(title=xaxis_column_name,
+                     type='linear' if xaxis_type == 'Linear' else 'log')
+
+    fig.update_yaxes(title=yaxis_column_name,
+                     type='linear' if yaxis_type == 'Linear' else 'log')
+
+    return fig
+
+# output is input for another callback
+
+
+@app.callback(
+    Output('cities-radio', 'options'),
+    Input('countries-radio', 'value'))
+def set_cities_options(selected_country):
+    return [{'label': i, 'value': i} for i in all_options[selected_country]]
+
+
+@app.callback(
+    Output('cities-radio', 'value'),
+    Input('cities-radio', 'options'))
+def set_cities_value(available_options):
+    return available_options[0]['value']
+
+
+@app.callback(
+    Output('display-selected-values', 'children'),
+    Input('countries-radio', 'value'),
+    Input('cities-radio', 'value'))
+def set_display_children(selected_country, selected_city):
+    return u'{} is a city in {}'.format(
+        selected_city, selected_country,
+    )
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
